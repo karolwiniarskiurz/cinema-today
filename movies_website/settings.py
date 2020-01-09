06752b2b298cc298acd15ad480a8b4e232590338
kwiniarski97 <kwiniarski97@gmail.com>
2020-01-09 20:53:22 +0100

    Added heroku support (#1)

--- movies_website/settings.py
+++ movies_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9w51ppjq*jq_#!vkym(hyqblsdxt5)39-#(u&@@@$ar)vw442='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+import django_heroku
+django_heroku.settings(locals())
